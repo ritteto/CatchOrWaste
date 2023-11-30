6fc8a14591b68879e2fd03b93a6b657a001d0f14
martincastromiguez <martin.castro@students.fhnw.ch>
2023-11-30 15:07:48 +0100

    Update: @Test1

---
+++ src/main/java/CatchOrWaste/Test1.java
@@ -0,0 +1,18 @@
+package CatchOrWaste;
+
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class Test1 extends GameApplication {
+
+
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
