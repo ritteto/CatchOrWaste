a2a59136368fa93bc5942739aa050362be1f4191
Maurin Wirth <dev.maurin@gmail.com>
2023-12-13 10:11:33 +0100

    Houses Position readded

--- src/main/java/com/pi4j/example/CatchOrWaste/Variables.java
+++ src/main/java/com/pi4j/example/CatchOrWaste/Variables.java
@@ -31,4 +31,14 @@ public class Variables {
 
     //Falling objects variables
     static int FALLING_OBJECT_AMOUNT = 3;
+
+    static double HOUSE1_X = getAppWidth()*0.62;
+    static double HOUSE1_Y = getAppHeight()*-0.001;
+    static double HOUSE2_X = getAppWidth()*0.48;
+    static double HOUSE2_Y = getAppHeight()*-0.001;
+    static double HOUSE3_X = getAppWidth()*0.34;
+    static double HOUSE3_Y = getAppHeight()*-0.001;
+    static double HOUSE4_X = getAppWidth()*0.20;
+    static double HOUSE4_Y = getAppHeight()*-0.001;
+
 }
