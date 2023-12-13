ac8792c9b575ec0a08405cf5bbd4f05d8d4615e5
Username1291 <96525979+Username1291@users.noreply.github.com>
2023-12-13 10:26:37 +0100

    fixed merge issues

--- src/main/java/com/pi4j/example/CatchOrWaste/SimpleFactory.java
+++ src/main/java/com/pi4j/example/CatchOrWaste/SimpleFactory.java
@@ -1,8 +1,5 @@
 package com.pi4j.example.CatchOrWaste;
-
-import com.almasb.fxgl.app.scene.GameView;
 import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.dsl.FXGLForKtKt;
 import com.almasb.fxgl.dsl.components.ProjectileComponent;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
@@ -10,19 +7,13 @@ import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.Spawns;
 import com.almasb.fxgl.physics.BoundingShape;
 import com.almasb.fxgl.physics.HitBox;
-import com.almasb.fxgl.physics.PolygonShapeData;
-import javafx.geometry.BoundingBox;
+
 import javafx.geometry.Point2D;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+
 
 import java.util.Random;
-
-import static com.almasb.fxgl.dsl.FXGLForKtKt.*;
 import static com.pi4j.example.CatchOrWaste.Variables.PLAYERSIZE;
-import static com.pi4j.example.CatchOrWaste.Variables.STREET_RIGHT;
-import static java.lang.Math.E;
-import static java.lang.Math.random;
+
 
 public class SimpleFactory implements EntityFactory {
 
