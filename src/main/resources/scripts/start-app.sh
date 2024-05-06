855a2cfab45cf7602fffe15ae2623cdf3a55fcf2
Maurin Wirth <maurin.wirth@students.fhnw.ch>
2024-05-06 13:18:00 +0200

    update: created scripts for autorun on RaspberryPi

---
+++ src/main/resources/scripts/start-app.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Java und JavaFX Konfiguration
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64  # Angepasster Pfad zur Java-Installation
+JAVAFX_SDK=/opt/javafx-sdk-20.0.2/lib             # Pfad zum JavaFX SDK
+
+# Starte das Spiel mit den richtigen JavaFX-Modulen und Eigenschaften
+#!/usr/bin/env bash
+
+# Setze Umgebungsvariablen
+export DISPLAY=:0
+export XAUTHORITY=/home/pi4j/.Xauthority
+
+# Starte die JavaFX-Anwendung
+sudo -E java -XX:+UseZGC -Xmx1G \
+  --module-path /opt/javafx-sdk/lib:/home/pi4j/deploy \
+  --add-modules javafx.controls \
+  -Dglass.platform=gtk \
+  --module com.pi4j.example/catchorwaste.CatchOrWasteApp
+
